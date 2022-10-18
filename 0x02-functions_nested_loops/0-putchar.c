ff71c58328a7fd705e0ece50ee80c602a3e59ca1
Abeltadesse19 <tadesseabel19@gmail.com>
2022-10-18 14:11:28 -0700

    loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,13 @@
  *    */
 int main(void)
 {
-		char *s = "_putchar"; /*arrays of characters or strings*/
-			int i = 0;
+		char *s = "_putchar";
 
-				while (s[i] != '\0')
-						{
-									_putchar(s[i]);
-											i++;
-												}
-					_putchar('\n');
+			for (int i = 0; i < 8; i++)
+				    {
+					    	    _putchar(s[i]);
+						        }
+				_putchar('\n');
 
-						return (0);
+					return (0);
 }
