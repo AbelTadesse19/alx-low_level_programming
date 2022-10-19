5a00e3b19015740a1ed616fe5253f66cfdd37ee7
Abeltadesse19 <tadesseabel19@gmail.com>
2022-10-19 10:58:02 -0700

    loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,20 @@
 #include "main.h"
+
 /**
- *  * _islower - Return 1 if letter is lowercase, 0 if not.
- *   * @c: char type letter
- *    * Return: 1 if lowercase, 0 otherwise lowercase
+ *  * main - check the code.
+ *   *
+ *    * Return: Always 0.
  *     */
-int _islower(int c)
+int main(void)
 {
-		/*if the character is between 96 and 123
-		 * 	ASCII decimals for characters*/
-		if (c > 96 && c < 123)
-					return (1);
-			else
-						return (0);
+	    int r;
+
+	        r = _islower('H');
+		    _putchar(r + '0');
+		        r = _islower('o');
+			    _putchar(r + '0');
+			        r = _islower(108);
+				    _putchar(r + '0');
+				        _putchar('\n');
+					    return (0);
 }
