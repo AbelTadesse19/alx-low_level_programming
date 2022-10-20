d141239abe621776f7e348cd9cff1f1586c0e41a
Abeltadesse19 <tadesseabel19@gmail.com>
2022-10-20 15:07:24 -0700

    more functions

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,35 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- *  * print_triangle - print a triangle aligned right, using '#'
- *   * @h: height of triangle
- *    */
+void print_triangle(int h);
+
+int main(void)
+{
+	    print_triangle(2);
+}
 
 void print_triangle(int h)
 {
 
-		int s, b;
-			s = 0;
-				b = h;
+	    int s, b;
+	        s = 0;
+		    b = h;
 
-				    /*for all i up to the forth */
-					for (int i = 0; i < h; i++)
-							{
-										s = s + 1;
-												b = b - 1;
-
-														for (int d = b; d > 0; d--)
-																	{
-																					_putchar(' ');
-																							}
-
-																for (int j = 0; j < s; j++)
-																			{
-																							_putchar('#');
-																									}
-
-																		_putchar('\n');
-																			}
-}
+		        // for all i up to the forth
+			//     for (int i = 0; i < h; i++)
+			//         {
+			//                 s = s + 1;
+			//                         b = b - 1;
+			//
+			//                                 for (int d = b; d > 0; d--)
+			//                                         {
+			//                                                     putchar(' ');
+			//                                                             }
+			//
+			//                                                                     for (int j = 0; j < s; j++)
+			//                                                                             {
+			//                                                                                         putchar('#');
+			//                                                                                                 }
+			//
+			//                                                                                                         putchar('\n');
+			//                                                                                                             }
+			//                                                                                                             }
