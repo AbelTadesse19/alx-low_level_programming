fcd4f2c77ab29d94b75e9165b3a05940a8cb00f4
Abeltadesse19 <tadesseabel19@gmail.com>
2022-10-20 14:35:24 -0700

    more functions

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,35 +1,35 @@
-#include <stdio.h>
+#include "main.h"
 
-void print_triangle(int h);
+/**
+ *  * print_triangle - print a triangle aligned right, using '#'
+ *   * @size: height of triangle
+ *    */
 
-int main(void)
+void print_triangle(int size)
 {
-	    print_triangle(2);
+
+		int s, b;
+
+			s = 0;
+
+				b = size;
+
+				    /*for all i up to the forth */
+					for (int i = 0; i < size; i++)
+							{
+										s = s + 1;
+												b = b - 1;
+
+														for (int d = b; d > 0; d--)
+																	{
+																					_putchar(' ');
+																							}
+
+																for (int j = 0; j < s; j++)
+																			{
+																							_putchar('#');
+																									}
+
+																		_putchar('\n');
+																			}
 }
-
-void print_triangle(int h)
-{
-
-	    int s, b;
-	        s = 0;
-		    b = h;
-
-		        // for all i up to the forth
-			//     for (int i = 0; i < h; i++)
-			//         {
-			//                 s = s + 1;
-			//                         b = b - 1;
-			//
-			//                                 for (int d = b; d > 0; d--)
-			//                                         {
-			//                                                     putchar(' ');
-			//                                                             }
-			//
-			//                                                                     for (int j = 0; j < s; j++)
-			//                                                                             {
-			//                                                                                         putchar('#');
-			//                                                                                                 }
-			//
-			//                                                                                                         putchar('\n');
-			//                                                                                                             }
-			//                                                                                                             }
