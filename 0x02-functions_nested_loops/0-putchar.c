c2a918ad5e2b9f45ae3ba3463b4c49c914c719a0
Abeltadesse19 <tadesseabel19@gmail.com>
2022-10-28 14:37:37 -0700

    02 function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- *  * main - prints out _putchar
- *   * Return: All good
- *    */
+ * main - prints out _putchar
+ * Return: All good
+ */
 int main(void)
 {
-		char *s = "_putchar"; /*arrays of characters or strings*/
-			int i = 0;
+	char *s = "_putchar"; /*arrays of characters or strings*/
+	int i = 0;
 
-				while (s[i] != '\0')
-						{
-									_putchar(s[i]);
-											i++;
-												}
-					_putchar('\n');
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
 
-						return (0);
+	return (0);
 }
