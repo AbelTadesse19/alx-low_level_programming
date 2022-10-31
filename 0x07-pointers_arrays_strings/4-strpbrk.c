99887aa07059b84562d544438812636d11569f9a
Abeltadesse19 <tadesseabel19@gmail.com>
2022-10-31 15:18:04 -0700

    07 pointers

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,31 +1,31 @@
-#include "main.h"                                                                                                   
-                                                                                                                    
-/**                                                                                                                 
- *_strpbk - search a string for any of set of bytes                                                                 
- *@s: source string                                                                                                 
- *@accept: accepted characters                                                                                      
- *                                                                                                                  
- *Return: the string since the first found accepted character                                                       
- */                                                                                                                 
-char *_strpbk(char *s, char *accept)                                                                                
-{                                                                                                                   
-        int a = 0, b;                                                                                               
-                                                                                                                    
-        while (s[a])                                                                                                
-        {                                                                                                           
-                b = 0;                                                                                              
-                                                                                                                    
-                while (accept[b])                                                                                   
-                {                                                                                                   
-                        if (s[a] == accept[b])                                                                      
-                        {                                                                                           
-                                s += a;                                                                             
-                                return (s);                                                                         
-                        }                                                                                           
-                        b++;                                                                                         
-                }                                                                                                   
-                a++;                                                                                                 
-        }                                                                                                           
-                                                                                                                    
-        return ('\0');
-}	
+#include "main.h"
+
+/**
+ * _strpbk - search a string for any of set of bytes
+ * @s: source string
+ * @accept: accepted characters
+ *
+ * Return: the string since the first found accepted character
+ */
+char *_strpbk(char *s, char *accept)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
+				s += a;
+				return (s);
+			}
+			b++;
+		}
+		a++;
+	}
+
+	return ('\0');
+}
