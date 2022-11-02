a9452055c33587ac106ec373e820d04eec1cd9bb
Abeltadesse19 <tadesseabel19@gmail.com>
2022-11-02 14:52:55 -0700

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,12 +9,14 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (y < 0) /*Base condition*/
+	{
 		return (-1);
-	else if (y == 1)
-		return (1);
-	else if (y == 1)
-		return (x);
+	}
 
-	return (x * _pow_recursion(x, y - 1));
+	else if (y != 0) /*recursive call*/
+		return (x * _pow_recursion(x, y - 1));
+
+	else
+		return (1);
 }
