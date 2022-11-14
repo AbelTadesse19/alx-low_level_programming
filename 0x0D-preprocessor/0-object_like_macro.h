993e926847b6b34dbef884bd8fdaee4cc5749860
Abeltadesse19 <tadesseabel19@gmail.com>
2022-11-14 10:40:30 -0800

    preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef OBJECT_LIKE_NARCO_H
+#define OBJECT_LIKE_NARCO_H
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "0-object_like_narco."
+#include "1-pi.h"
+#include "3-function_like_narco.h"
+#include "4-sum.h"
+
+#endif 
