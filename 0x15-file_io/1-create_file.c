244c17a087ddf0107e85d6c3f1b1a4d018cd6622
Abeltadesse19 <tadesseabel19@gmail.com>
2022-12-05 13:33:39 -0800

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,48 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * create_file - Creates a file
- * @filename: pointer to the name of the file to create
- * @text_content: pointer to a string to write to the file
+ * _strlen - function that returns the length of a string.
  *
- * Return: if the function fails - -1
- * otherwise - 1
+ * @s: pointer to an string
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i += 1;
+	}
+	return (i);
+}
+
+/**
+ * create_file - function that create a file
+ * @filename: name of file to be created
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int fd, fd_write;
 
-	if (filename)
+	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-
-	if (text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
+	if (text_content == NULL)
+		return (1);
+	fd_write = write(fd, text_content, _strlen(text_content));
 	close(fd);
-
-	return (-1);
+	if (fd_write == -1)
+		return (-1);
+	return (1);
 }
